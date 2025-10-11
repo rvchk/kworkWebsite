7d38507ae91d4e788b5776bd515d2b89ae70a917
Matvei Rovchak <115169534+rvchk@users.noreply.github.com>
2025-10-11 22:54:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const starIcon = star.querySelector('.star-icon');
       
       // Меняем иконку на желтую звезду
-      starIcon.src = '/assets/images/icons/yellow-star.png';
+      starIcon.src = './assets/images/icons/yellow-star.png';
       star.classList.add('lit');
       
       // Анимация появления текста
@@ -158,7 +158,7 @@ document.addEventListener('DOMContentLoaded', function() {
     stars.forEach(star => {
       star.classList.remove('lit');
       const starIcon = star.querySelector('.star-icon');
-      starIcon.src = '/assets/images/icons/star.svg';
+      starIcon.src = './assets/images/icons/star.svg';
       const starText = star.querySelector('.star-text');
       starText.style.opacity = '0';
       starText.style.transform = 'translateY(10px)';
